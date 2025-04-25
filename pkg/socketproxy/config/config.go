25c13b08ad9058eb25e7a6b3f1cd250421a127b7
Walter Scott <w@lter.ca>
2025-04-25 11:27:57 -0500

    add socket proxy

---
+++ pkg/socketproxy/config/config.go
@@ -0,0 +1,115 @@
+package config
+
+import (
+	"errors"
+	"fmt"
+	"net"
+	"net/http"
+	"os"
+	"regexp"
+)
+
+var (
+	defaultAllowFrom                   = "127.0.0.1/32"         // allowed IPs to connect to the proxy
+	defaultAllowHealthcheck            = false                  // allow health check requests (HEAD http://localhost:55555/health)
+	defaultLogJSON                     = false                  // if true, log in JSON format
+	defaultLogLevel                    = "INFO"                 // log level as string
+	defaultListenIP                    = "127.0.0.1"            // ip address to bind the server to
+	defaultProxyPort                   = uint(2375)             // tcp port to listen on
+	defaultSocketPath                  = "/var/run/docker.sock" // path to the unix socket
+	defaultShutdownGraceTime           = uint(10)               // Maximum time in seconds to wait for the server to shut down gracefully
+	defaultWatchdogInterval            = uint(0)                // watchdog interval in seconds (0 to disable)
+	defaultStopOnWatchdog              = false                  // set to true to stop the program when the socket gets unavailable (otherwise log only)
+	defaultProxySocketEndpoint         = ""                     // empty string means no socket listener, but regular TCP listener
+	defaultProxySocketEndpointFileMode = uint(0o400)            // set the file mode of the unix socket endpoint
+)
+
+type Config struct {
+	AllowedRequests             map[string]*regexp.Regexp
+	AllowFrom                   string
+	AllowHealthcheck            bool
+	StopOnWatchdog              bool
+	ShutdownGraceTime           uint
+	WatchdogInterval            uint
+	ListenIP                    string
+	ProxyPort                   uint
+	SocketPath                  string
+	ProxySocketEndpoint         string
+	ProxySocketEndpointFileMode os.FileMode
+}
+
+// used for list of allowed requests
+type methodRegex struct {
+	method               string
+	regexStringFromEnv   string
+	regexStringFromParam string
+}
+
+// mr is the allowlist of requests per http method
+// default: regexStringFromEnv and regexStringFromParam are empty, so regexCompiled stays nil and the request is blocked
+// if regexStringParam is set with a command line parameter, all requests matching the method and path matching the regex are allowed
+// else if regexStringEnv from Environment ist checked
+var mr = []methodRegex{
+	{method: http.MethodGet},
+	{method: http.MethodHead},
+	{method: http.MethodPost},
+	{method: http.MethodPut},
+	{method: http.MethodPatch},
+	{method: http.MethodDelete},
+	{method: http.MethodConnect},
+	{method: http.MethodTrace},
+	{method: http.MethodOptions},
+}
+
+func NewDefaultConfig() *Config {
+	return &Config{
+		AllowedRequests:             make(map[string]*regexp.Regexp),
+		AllowFrom:                   defaultAllowFrom,
+		AllowHealthcheck:            defaultAllowHealthcheck,
+		ListenIP:                    defaultListenIP,
+		ProxyPort:                   defaultProxyPort,
+		SocketPath:                  defaultSocketPath,
+		ProxySocketEndpoint:         defaultProxySocketEndpoint,
+		ProxySocketEndpointFileMode: os.FileMode(defaultProxySocketEndpointFileMode),
+		ShutdownGraceTime:           defaultShutdownGraceTime,
+		WatchdogInterval:            defaultWatchdogInterval,
+		StopOnWatchdog:              defaultStopOnWatchdog,
+	}
+}
+
+func (cfg *Config) ListenAddress() string {
+	return fmt.Sprintf("%s:%d", cfg.ListenIP, cfg.ProxyPort)
+}
+
+func (cfg *Config) Validate() error {
+
+	if net.ParseIP(cfg.ListenIP) == nil {
+		return fmt.Errorf("invalid IP \"%s\" for listenip", cfg.ListenIP)
+	}
+	if cfg.ProxyPort < 1 || cfg.ProxyPort > 65535 {
+		return errors.New("port number has to be between 1 and 65535")
+	}
+
+	if cfg.ProxySocketEndpointFileMode > 0o777 {
+		return errors.New("file mode has to be between 0 and 0o777")
+	}
+
+	// compile regexes for allowed requests
+	cfg.AllowedRequests = make(map[string]*regexp.Regexp)
+	for _, rx := range mr {
+		if rx.regexStringFromParam != "" {
+			r, err := regexp.Compile("^" + rx.regexStringFromParam + "$")
+			if err != nil {
+				return fmt.Errorf("invalid regex \"%s\" for method %s in command line parameter: %w", rx.regexStringFromParam, rx.method, err)
+			}
+			cfg.AllowedRequests[rx.method] = r
+		} else if rx.regexStringFromEnv != "" {
+			r, err := regexp.Compile("^" + rx.regexStringFromEnv + "$")
+			if err != nil {
+				return fmt.Errorf("invalid regex \"%s\" for method %s in env variable: %w", rx.regexStringFromParam, rx.method, err)
+			}
+			cfg.AllowedRequests[rx.method] = r
+		}
+	}
+	return nil
+}
---
+++ pkg/socketproxy/handlehttprequest.go
@@ -0,0 +1,89 @@
+package socketproxy
+
+import (
+	"errors"
+	"log/slog"
+	"net"
+	"net/http"
+	"strings"
+)
+
+// handleHTTPRequest checks if the request is allowed and sends it to the proxy.
+// Otherwise, it returns a "405 Method Not Allowed" or a "403 Forbidden" error.
+// In case of an error, it returns a 500 Internal Server Error.
+func handleHTTPRequest(w http.ResponseWriter, r *http.Request) {
+	if cfg.ProxySocketEndpoint == "" { // do not perform this check if we proxy to a unix socket
+		allowedIP, err := isAllowedClient(r.RemoteAddr)
+		if err != nil {
+			slog.Warn("cannot get valid IP address for client allowlist check", "reason", err, "method", r.Method, "URL", r.URL, "client", r.RemoteAddr)
+		}
+		if !allowedIP {
+			communicateBlockedRequest(w, r, "forbidden IP", http.StatusForbidden)
+			return
+		}
+	}
+
+	// check if the request is allowed
+	allowed, exists := cfg.AllowedRequests[r.Method]
+	if !exists { // method not in map -> not allowed
+		communicateBlockedRequest(w, r, "method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	if !allowed.MatchString(r.URL.Path) { // path does not match regex -> not allowed
+		communicateBlockedRequest(w, r, "path not allowed", http.StatusForbidden)
+		return
+	}
+
+	// finally log and proxy the request
+	slog.Debug("allowed request", "method", r.Method, "URL", r.URL, "client", r.RemoteAddr)
+	socketProxy.ServeHTTP(w, r) // proxy the request
+}
+
+// isAllowedClient checks if the given remote address is allowed to connect to the proxy.
+// The IP address is extracted from a RemoteAddr string (the part before the colon).
+func isAllowedClient(remoteAddr string) (bool, error) {
+	// Get the client IP address from the remote address string
+	clientIPStr, _, err := net.SplitHostPort(remoteAddr)
+	if err != nil {
+		return false, err
+	}
+	// Parse the IP address
+	clientIP := net.ParseIP(clientIPStr)
+	if clientIP == nil {
+		return false, errors.New("invalid IP format")
+	}
+
+	_, allowedIPNet, err := net.ParseCIDR(cfg.AllowFrom)
+	if err == nil {
+		// AllowFrom is a valid CIDR, so check if IP address is in allowed network
+		return allowedIPNet.Contains(clientIP), nil
+	}
+
+	// AllowFrom is not a valid CIDR, so try to resolve it via DNS
+	// split over comma to support multiple hostnames
+	allowFromList := strings.Split(cfg.AllowFrom, ",")
+	for _, allowFrom := range allowFromList {
+		ips, err := net.LookupIP(allowFrom)
+		if err != nil {
+			slog.Warn("error looking up allowed client hostname", "hostname", allowFrom, "error", err.Error())
+		}
+		for _, ip := range ips {
+			// Check if IP address is one of the resolved IPs
+			if ip.Equal(clientIP) {
+				return true, nil
+			}
+		}
+	}
+	return false, nil
+}
+
+// sendHTTPError sends a HTTP error with the given status code.
+func sendHTTPError(w http.ResponseWriter, status int) {
+	http.Error(w, http.StatusText(status), status)
+}
+
+// communicateBlockedRequest logs a blocked request and sends a HTTP error.
+func communicateBlockedRequest(w http.ResponseWriter, r *http.Request, reason string, status int) {
+	slog.Warn("blocked request", "reason", reason, "method", r.Method, "URL", r.URL, "client", r.RemoteAddr, "response", status)
+	sendHTTPError(w, status)
+}
