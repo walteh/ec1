dbc3c9fa1ff25efbb6a7bbade4b7eadd8179c2f6
Walter Scott <w@lter.ca>
2025-05-14 08:48:29 -0500

    undo panic

--- pkg/fc/api.go
+++ pkg/fc/api.go
@@ -17,146 +17,145 @@ type EC1FirecrackerAPI struct {
 
 // CreateSnapshot implements operations.FirecrackerAPI.
 func (f *EC1FirecrackerAPI) CreateSnapshot(ctx context.Context, params operations.CreateSnapshotParams) operations.CreateSnapshotResponder {
-	panic("unimplemented")
+	return operations.CreateSnapshotNotImplemented()
 }
 
 // CreateSyncAction implements operations.FirecrackerAPI.
 func (f *EC1FirecrackerAPI) CreateSyncAction(ctx context.Context, params operations.CreateSyncActionParams) operations.CreateSyncActionResponder {
-	panic("unimplemented")
+	return operations.CreateSyncActionNotImplemented()
 }
 
 // DescribeBalloonConfig implements operations.FirecrackerAPI.
 func (f *EC1FirecrackerAPI) DescribeBalloonConfig(ctx context.Context, params operations.DescribeBalloonConfigParams) operations.DescribeBalloonConfigResponder {
-	panic("unimplemented")
+	return operations.DescribeBalloonConfigNotImplemented()
 }
 
 // DescribeBalloonStats implements operations.FirecrackerAPI.
 func (f *EC1FirecrackerAPI) DescribeBalloonStats(ctx context.Context, params operations.DescribeBalloonStatsParams) operations.DescribeBalloonStatsResponder {
-	panic("unimplemented")
+	return operations.DescribeBalloonStatsNotImplemented()
 }
 
 // DescribeInstance implements operations.FirecrackerAPI.
 func (f *EC1FirecrackerAPI) DescribeInstance(ctx context.Context, params operations.DescribeInstanceParams) operations.DescribeInstanceResponder {
-
-	panic("unimplemented")
+	return operations.DescribeInstanceNotImplemented()
 }
 
 // GetExportVMConfig implements operations.FirecrackerAPI.
 func (f *EC1FirecrackerAPI) GetExportVMConfig(ctx context.Context, params operations.GetExportVMConfigParams) operations.GetExportVMConfigResponder {
-	panic("unimplemented")
+	return operations.GetExportVMConfigNotImplemented()
 }
 
 // GetFirecrackerVersion implements operations.FirecrackerAPI.
 func (f *EC1FirecrackerAPI) GetFirecrackerVersion(ctx context.Context, params operations.GetFirecrackerVersionParams) operations.GetFirecrackerVersionResponder {
-	panic("unimplemented")
+	return operations.GetFirecrackerVersionNotImplemented()
 }
 
 // GetMachineConfiguration implements operations.FirecrackerAPI.
 func (f *EC1FirecrackerAPI) GetMachineConfiguration(ctx context.Context, params operations.GetMachineConfigurationParams) operations.GetMachineConfigurationResponder {
-	panic("unimplemented")
+	return operations.GetMachineConfigurationNotImplemented()
 }
 
 // GetMmds implements operations.FirecrackerAPI.
 func (f *EC1FirecrackerAPI) GetMmds(ctx context.Context, params operations.GetMmdsParams) operations.GetMmdsResponder {
-	panic("unimplemented")
+	return operations.GetMmdsNotImplemented()
 }
 
 // LoadSnapshot implements operations.FirecrackerAPI.
 func (f *EC1FirecrackerAPI) LoadSnapshot(ctx context.Context, params operations.LoadSnapshotParams) operations.LoadSnapshotResponder {
-	panic("unimplemented")
+	return operations.LoadSnapshotNotImplemented()
 }
 
 // PatchBalloon implements operations.FirecrackerAPI.
 func (f *EC1FirecrackerAPI) PatchBalloon(ctx context.Context, params operations.PatchBalloonParams) operations.PatchBalloonResponder {
-	panic("unimplemented")
+	return operations.PatchBalloonNotImplemented()
 }
 
 // PatchBalloonStatsInterval implements operations.FirecrackerAPI.
 func (f *EC1FirecrackerAPI) PatchBalloonStatsInterval(ctx context.Context, params operations.PatchBalloonStatsIntervalParams) operations.PatchBalloonStatsIntervalResponder {
-	panic("unimplemented")
+	return operations.PatchBalloonStatsIntervalNotImplemented()
 }
 
 // PatchGuestDriveByID implements operations.FirecrackerAPI.
 func (f *EC1FirecrackerAPI) PatchGuestDriveByID(ctx context.Context, params operations.PatchGuestDriveByIDParams) operations.PatchGuestDriveByIDResponder {
-	panic("unimplemented")
+	return operations.PatchGuestDriveByIDNotImplemented()
 }
 
 // PatchGuestNetworkInterfaceByID implements operations.FirecrackerAPI.
 func (f *EC1FirecrackerAPI) PatchGuestNetworkInterfaceByID(ctx context.Context, params operations.PatchGuestNetworkInterfaceByIDParams) operations.PatchGuestNetworkInterfaceByIDResponder {
-	panic("unimplemented")
+	return operations.PatchGuestNetworkInterfaceByIDNotImplemented()
 }
 
 // PatchMachineConfiguration implements operations.FirecrackerAPI.
 func (f *EC1FirecrackerAPI) PatchMachineConfiguration(ctx context.Context, params operations.PatchMachineConfigurationParams) operations.PatchMachineConfigurationResponder {
-	panic("unimplemented")
+	return operations.PatchMachineConfigurationNotImplemented()
 }
 
 // PatchMmds implements operations.FirecrackerAPI.
 func (f *EC1FirecrackerAPI) PatchMmds(ctx context.Context, params operations.PatchMmdsParams) operations.PatchMmdsResponder {
-	panic("unimplemented")
+	return operations.PatchMmdsNotImplemented()
 }
 
 // PatchVM implements operations.FirecrackerAPI.
 func (f *EC1FirecrackerAPI) PatchVM(ctx context.Context, params operations.PatchVMParams) operations.PatchVMResponder {
-	panic("unimplemented")
+	return operations.PatchVMNotImplemented()
 }
 
 // PutBalloon implements operations.FirecrackerAPI.
 func (f *EC1FirecrackerAPI) PutBalloon(ctx context.Context, params operations.PutBalloonParams) operations.PutBalloonResponder {
-	panic("unimplemented")
+	return operations.PutBalloonNotImplemented()
 }
 
 // PutCPUConfiguration implements operations.FirecrackerAPI.
 func (f *EC1FirecrackerAPI) PutCPUConfiguration(ctx context.Context, params operations.PutCPUConfigurationParams) operations.PutCPUConfigurationResponder {
-	panic("unimplemented")
+	return operations.PutCPUConfigurationNotImplemented()
 }
 
 // PutEntropyDevice implements operations.FirecrackerAPI.
 func (f *EC1FirecrackerAPI) PutEntropyDevice(ctx context.Context, params operations.PutEntropyDeviceParams) operations.PutEntropyDeviceResponder {
-	panic("unimplemented")
+	return operations.PutEntropyDeviceNotImplemented()
 }
 
 // PutGuestBootSource implements operations.FirecrackerAPI.
 func (f *EC1FirecrackerAPI) PutGuestBootSource(ctx context.Context, params operations.PutGuestBootSourceParams) operations.PutGuestBootSourceResponder {
-	panic("unimplemented")
+	return operations.PutGuestBootSourceNotImplemented()
 }
 
 // PutGuestDriveByID implements operations.FirecrackerAPI.
 func (f *EC1FirecrackerAPI) PutGuestDriveByID(ctx context.Context, params operations.PutGuestDriveByIDParams) operations.PutGuestDriveByIDResponder {
-	panic("unimplemented")
+	return operations.PutGuestDriveByIDNotImplemented()
 }
 
 // PutGuestNetworkInterfaceByID implements operations.FirecrackerAPI.
 func (f *EC1FirecrackerAPI) PutGuestNetworkInterfaceByID(ctx context.Context, params operations.PutGuestNetworkInterfaceByIDParams) operations.PutGuestNetworkInterfaceByIDResponder {
-	panic("unimplemented")
+	return operations.PutGuestNetworkInterfaceByIDNotImplemented()
 }
 
 // PutGuestVsock implements operations.FirecrackerAPI.
 func (f *EC1FirecrackerAPI) PutGuestVsock(ctx context.Context, params operations.PutGuestVsockParams) operations.PutGuestVsockResponder {
-	panic("unimplemented")
+	return operations.PutGuestVsockNotImplemented()
 }
 
 // PutLogger implements operations.FirecrackerAPI.
 func (f *EC1FirecrackerAPI) PutLogger(ctx context.Context, params operations.PutLoggerParams) operations.PutLoggerResponder {
-	panic("unimplemented")
+	return operations.PutLoggerNotImplemented()
 }
 
 // PutMachineConfiguration implements operations.FirecrackerAPI.
 func (f *EC1FirecrackerAPI) PutMachineConfiguration(ctx context.Context, params operations.PutMachineConfigurationParams) operations.PutMachineConfigurationResponder {
-	panic("unimplemented")
+	return operations.PutMachineConfigurationNotImplemented()
 }
 
 // PutMetrics implements operations.FirecrackerAPI.
 func (f *EC1FirecrackerAPI) PutMetrics(ctx context.Context, params operations.PutMetricsParams) operations.PutMetricsResponder {
-	panic("unimplemented")
+	return operations.PutMetricsNotImplemented()
 }
 
 // PutMmds implements operations.FirecrackerAPI.
 func (f *EC1FirecrackerAPI) PutMmds(ctx context.Context, params operations.PutMmdsParams) operations.PutMmdsResponder {
-	panic("unimplemented")
+	return operations.PutMmdsNotImplemented()
 }
 
 // PutMmdsConfig implements operations.FirecrackerAPI.
 func (f *EC1FirecrackerAPI) PutMmdsConfig(ctx context.Context, params operations.PutMmdsConfigParams) operations.PutMmdsConfigResponder {
-	panic("unimplemented")
+	return operations.PutMmdsConfigNotImplemented()
 }
